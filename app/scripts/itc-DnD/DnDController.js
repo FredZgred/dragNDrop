6518c521ed743d002cf56e48e6a3cc010715c2f2
Szymon Maslowski <szymon.maslowski@vp.pl>
2015-03-10 00:05:43 +0100

    hmhm

--- app/scripts/itc-DnD/DnDController.js
+++ app/scripts/itc-DnD/DnDController.js
@@ -3,61 +3,6 @@
     'use strict';
 
     function controller( $scope ) {
-
-        function removeCols ( num ) {
-            num = Math.abs( num );
-
-            // deleting empty columns
-            for( var i=0; i<$scope.structure.columns.length; i++ ) {
-                if( $scope.structure.columns[ i ].length === 0 ) {
-                    $scope.structure.columns.splice( i, 1 );
-                    --num;
-                }
-                if( num === 0 ) {
-                    break;
-                }
-            }
-
-            // deleting filled columns when there was not enough empty columns
-            if( num > 0 ) {
-                for( var j=0; j<num; j++ ) {
-                    angular.forEach( $scope.structure.columns[ $scope.structure.columns.length-1 ], function( elem ) {
-                        $scope.structure.columns[ $scope.structure.columns.length-2 ].push( elem );
-                    } );
-                    $scope.structure.columns.splice( $scope.structure.columns.length-1, 1 );
-                }
-            }
-        }
-
-        $scope.renderColumns = function( num ) {
-            if( num > 0 ) {
-                for( var i=0; i<num; i++ ) {
-                    $scope.structure.columns.push( [] );
-                }
-            }
-            else {
-                removeCols( num );
-            }
-        };
-
-
-        $scope.getJson = function() {
-            $scope.json = angular.toJson( $scope.structure );
-        };
-
-
-        $scope.updateFromJson = function() {
-            $scope.structure = JSON.parse( $scope.json );
-            if( $scope.structure.dropAreasCount < 3 ) {
-                $scope.structure.dropAreasCount = 3;
-            }
-        };
-
-
-        $scope.sortableOptions = {
-            placeholder: 'btn',
-            connectWith: '.droppable-area'
-        };
     }
 
     angular.module( 'itc-DnD' )
--- app/scripts/itc-DnD/DnDDirective.js
+++ app/scripts/itc-DnD/DnDDirective.js
@@ -4,40 +4,80 @@
 
     function link( scope ) {
 
-        scope.structure = {
-            dropAreasCount: 4,
-            unallocated: [
-                { title: 'firstElem', type: 0 },
-                { title: 'secondElem', type: 0 },
-                { title: 'thirdElem', type: 0 },
-                { title: 'sixthElem', type: 0 },
-                { title: 'seventhElem', type: 0 }
-            ],
-            columns: [
-                [
-                    { title: 'ninthElem', type: 0 },
-                    { title: 'tenthElem', type: 0 }
-                ],
-                [
-                    { title: 'eighthElem', type: 0 }
-                ],
-                [],
-                [
-                    { title: 'fourthElem', type: 0 },
-                    { title: 'fifthElem', type: 0 }
-                ]
-            ]
+        var clearWatch;
+        scope.structure = {};
+
+
+        function removeCols ( num ) {
+            num = Math.abs( num );
+
+            // deleting empty columns
+            for( var i=0; i<scope.structure.columns.length; i++ ) {
+                if( scope.structure.columns[ i ].length === 0 ) {
+                    scope.structure.columns.splice( i, 1 );
+                    --num;
+                }
+                if( num === 0 ) {
+                    break;
+                }
+            }
+
+            // deleting filled columns when there was not enough empty columns
+            if( num > 0 ) {
+                for( var j=0; j<num; j++ ) {
+                    angular.forEach( scope.structure.columns[ scope.structure.columns.length-1 ], function( elem ) {
+                        scope.structure.columns[ scope.structure.columns.length-2 ].push( elem );
+                    } );
+                    scope.structure.columns.splice( scope.structure.columns.length-1, 1 );
+                }
+            }
+        }
+
+        function renderColumns( num ) {
+            if( num > 0 ) {
+                for( var i=0; i<num; i++ ) {
+                    scope.structure.columns.push( [] );
+                }
+            }
+            else {
+                removeCols( num );
+            }
+        }
+
+        function setWatch( lastDACount ) {
+            lastDACount = lastDACount || 0;
+            clearWatch = scope.$watch( function() {
+                return scope.structure.dropAreasCount;
+            }, function( newV, oldV ) {
+                if( ( newV>=3 && newV<oldV ) || ( newV>=4 && newV>oldV ) ) {
+                    renderColumns( newV - ( oldV || lastDACount ) );
+                }
+            }, true );
+        }
+
+
+        scope.getJson = function() {
+            scope.json = angular.toJson( scope.structure );
         };
 
-        console.log({"dropAreasCount":5,"unallocated":[{"title":"firstElem","type":0},{"title":"secondElem","type":0},{"title":"thirdElem","type":0},{"title":"sixthElem","type":0},{"title":"seventhElem","type":0}],"columns":[[{"title":"ninthElem","type":0},{"title":"tenthElem","type":0}],[{"title":"eighthElem","type":0}],[],[{"title":"fourthElem","type":0},{"title":"fifthElem","type":0}],[]]});
 
-        scope.$watch( function() {
-            return scope.structure.dropAreasCount;
-        }, function( newV, oldV ) {
-            if( ( newV>=3 && newV<oldV ) || ( newV>=4 && newV>oldV ) ) {
-                scope.renderColumns( newV - ( oldV || 0 ) );
+        scope.updateFromJson = function() {
+            var lastDACount = scope.structure.dropAreasCount;
+            clearWatch();
+            scope.structure = JSON.parse( scope.json );
+            if( scope.structure.dropAreasCount < 3 ) {
+                scope.structure.dropAreasCount = 3;
             }
-        }, true );
+            setWatch( lastDACount );
+        };
+
+
+        scope.sortableOptions = {
+            placeholder: 'btn',
+            connectWith: '.droppable-area'
+        };
+
+        setWatch();
     }
 
     function directive() {
@@ -45,7 +85,6 @@
             restrict: 'E',
             scope: {},
             link: link,
-            controller: 'DnDController',
             templateUrl: 'scripts/itc-DnD/DnDTemplate.html'
         };
     }
